38f10f10515118c64a8f6ddc81e3ec62e113e2b9
SaulReznik <robhov97@gmail.com>
2019-09-17 21:33:10 +0400

    Done with adding and displaying products

--- src/components/routes/Products.js
+++ src/components/routes/Products.js
@@ -9,22 +9,17 @@ export default class Products extends React.Component{
                 name: "",
                 amount: 0
             },
-            products: {
+            products: [
                 
-            }
+            ]
         }
 
         this.toggleInputs = this.toggleInputs.bind(this);
+        this.handleName = this.handleName.bind(this);
+        this.handleAmount = this.handleAmount.bind(this);
         this.minus = this.minus.bind(this);
         this.plus = this.plus.bind(this);
-    }
-
-    minus() {
-        this.setState({ newItem: { ...this.state.newItem, amount: this.state.newItem.amount - 1}})
-    }
-
-    plus() {
-        this.setState({ newItem: { ...this.state.newItem, amount: this.state.newItem.amount + 1}})
+        this.handleSubmit = this.handleSubmit.bind(this);
     }
 
     toggleInputs() {
@@ -37,29 +32,75 @@ export default class Products extends React.Component{
         }
     }
 
+    handleName(e) {
+        this.setState({ newItem: { ...this.state.newItem, name: e.target.value } })
+    }
+
+    handleAmount(e) {
+        this.setState({ newItem: { ...this.state.newItem, amount: e.target.value } })
+    }
+
+    minus() {
+        this.setState({ newItem: { ...this.state.newItem, amount: this.state.newItem.amount - 1}})
+    }
+
+    plus() {
+        this.setState({ newItem: { ...this.state.newItem, amount: this.state.newItem.amount + 1}})
+    }
+
+    handleSubmit() {
+        let singleProduct = {
+            name: this.state.newItem.name,
+            amount: this.state.newItem.amount
+        }
+        this.setState({
+            products: [...this.state.products, singleProduct],
+            newItem: { name: "", amount: 0 }
+        });
+    }
+
     render(){
         return(
             <div>
+
                 <h1>Products</h1>
                 <button onClick={this.toggleInputs} id="enter-new-product-button">Enter New Product</button>
                 
                 <div id="add-product-container">
-                    <div id="inputs-container">
+                    <div id="inputs-container" onSubmit={this.handleSubmit}>
                         <input 
-                            onChange={(e) => { this.setState({ newItem: { ...this.state.newItem, name: e.target.value }})}} 
+                            onChange={this.handleName} 
                             value={this.state.newItem.name} 
                             id="new-product-input" 
                             type="text" 
                         />
                         <button onClick={this.minus} className="plus-minus-buttons" id="minus">-</button>
                         <input 
-                            onChange={(e) => { this.setState({ newItem: { ...this.state.newItem, amount: e.target.value } })}} 
+                            onChange={this.handleAmount} 
                             value={this.state.newItem.amount} 
                             type="number" 
                         />
                         <button onClick={this.plus} className="plus-minus-buttons" id="plus">+</button>
+
+                        <button onClick={this.handleSubmit} id="submit-button">Add New Product</button>
                     </div>
                 </div>
+
+                {
+                    this.state.products.map((item, index) => {
+                        return(
+                            <div key={index} className="inputs-container">
+                                <input readOnly value={this.state.products[index].name} className="new-product-inputs" type="text" />
+                                <div className="inputs-amount-container">
+                                    <button className="subtraction-button">-</button>
+                                    <input readOnly value={this.state.products[index].amount} className="amount-input" type="number" />
+                                    <button className="addition-button">+</button>
+                                </div>
+                            </div>
+                        )
+                    })
+                }
+
             </div>
         )
     }
