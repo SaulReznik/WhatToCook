f9e79f556840272bdb800e97cd89cc3b281c6fe7
SaulReznik <robhov97@gmail.com>
2021-02-07 18:41:10 +0400

    Sidebar and Recipe are transformed into FC

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -2,23 +2,21 @@ import React from 'react';
 import "../styles/Sidebar.css";
 import { Link } from 'react-router-dom';
 
-export default class Sidebar extends React.Component{
-    render(){
-        return(
-            <div id="sidebar">
-                <h1>Sidebar</h1>
-                <div id="links">
-                    <div className="link-container">
-                        <Link to='/'>What To Cook</Link>
-                    </div>
-                    <div className="link-container">
-                        <Link to='/products'>Products</Link>
-                    </div>
-                    <div className="link-container">
-                        <Link to='/recipes'>Recipes</Link>
-                    </div>
-                </div>
+const Sidebar = () => (
+    <div id="sidebar">
+        <h1>Sidebar</h1>
+        <div id="links">
+            <div className="link-container">
+                <Link to='/'>What To Cook</Link>
             </div>
-        )
-    }
-}
+            <div className="link-container">
+                <Link to='/products'>Products</Link>
+            </div>
+            <div className="link-container">
+                <Link to='/recipes'>Recipes</Link>
+            </div>
+        </div>
+    </div>
+)
+
+export default Sidebar;
